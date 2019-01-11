d88e9b1ccac7f296cbf69422436adfb5b10dcc16
zuoyuan <zuoyuan@meituan.com>
2019-01-11 13:14:40 +0800

    add deepctr getstart

---
+++ deep-ctr/getstart/w_d_model.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+import tensorflow as tf
+
+
+
+class WideDeep(object):
+    def __init__(self):
+        # TODO 初始化计算图
+        raise NotImplementedError()
+
+    def fit(self, data):
+        # TODO 模型训练
+        raise NotImplementedError()
+
+    def predict(self, data):
+        # TODO 预测
+        raise NotImplementedError()
+
+    def eval(self, data):
+        # TODO 模型评估
+        raise NotImplementedError()
