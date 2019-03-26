b980feb18430c858fb84bfb15036c732d48ca298
zhangweite <zhangweite@admindeMacBook-Pro.local>
2019-03-26 16:09:38 +0800

    add code

--- basic-tf/zhangweite/dnn.py
+++ basic-tf/zhangweite/dnn.py
@@ -5,8 +5,6 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-
-## 创建计算图中的关键节点
 with tf.name_scope('input'):
     tf_x = tf.placeholder(tf.float32, [None, 784])
     tf_y = tf.placeholder(tf.float32, [None, 10])
@@ -61,30 +59,25 @@ def rnn_net(x_):
     return state
 
 # mlp
-# output = mlp_net(tf_x)
+output = mlp_net(tf_x)
 
 # cnn
 # output = conv_net(tf_x)
 
 # rnn
-state = rnn_net(tf_x)
-output = tf.layers.dense(state, 10)  # [bacth,10]   
+# state = rnn_net(tf_x)
+# output = tf.layers.dense(state, 10)  # [bacth,10]   
 
 
-## 定义损失函数
 with tf.name_scope('loss'):
-    ## TODO 定义交叉熵
     Loss = tf.losses.softmax_cross_entropy(onehot_labels=tf_y, logits=output) 
     corr = tf.equal(tf.argmax(output, 1), tf.argmax(tf_y,1))
     acc = tf.reduce_mean(tf.cast(corr, tf.float32))
 
-## TODO 定义一个step全局变量和优化器,并指定 global_step
 step = tf.Variable(0, trainable=False)
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(Loss, global_step=step)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(Loss, global_step=step)
 update_op = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
-
-## TODO 添加summary: loss, acc
 
 tf.summary.scalar('loss', Loss)
 tf.summary.scalar('acc', acc)
@@ -92,30 +85,24 @@ merged = tf.summary.merge_all()
 
 
 with tf.Session() as sess:
-    ## TODO 定义训练集和测试集日志writer
-    logdir = './tmp/log_rnn'
+    logdir = './tmp/log_mlp'
     train_writer = tf.summary.FileWriter(logdir + '/train', sess.graph)
     test_writer = tf.summary.FileWriter(logdir + '/test')
 
-    ## TODO 初始化全局变量
     sess.run(tf.group(tf.global_variables_initializer(), tf.local_variables_initializer()))
    
-    ## 定义模型checkpoint保存对象, 检查路径是否存在, 如果存在就从checkpoint文件中恢复
     saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES))
-    ckpt_path = './tmp/mnist_rnn/model.ckpt'
+    ckpt_path = './tmp/mnist_mlp/model.ckpt'
     if os.path.exists(os.path.dirname(ckpt_path)):
         saver.restore(sess, ckpt_path)
     else:
         os.mkdir(os.path.dirname(ckpt_path))
 
-    ## 训练模型
     for _ in range(10000):
         xs, ys = mnist.train.next_batch(100)
-        ##TODO 训练模型
         loss, i, _, _ = sess.run([Loss, step, optimizer, update_op], 
             feed_dict = {tf_x: xs,tf_y: ys, tf_is_train: True})
         if i % 1000 == 0:
-            ## TODO 评估模型在训练集和测试集上的效果,并将日志保存到日志文件中
             acc_train, summary = sess.run([acc, merged], feed_dict={tf_x: mnist.train.images, tf_y: mnist.train.labels, tf_is_train: False})
             train_writer.add_summary(summary, global_step=i)
 
@@ -126,5 +113,4 @@ with tf.Session() as sess:
                 'train acc', acc_train, \
                 'test acc', acc_test)
 
-            ## TODO 保存模型
             saver.save(sess, ckpt_path)
