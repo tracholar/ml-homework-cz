938d41e4ecff3e902c80ed36189e08e3a740626e
zuoyuan <zuoyuan@meituan.com>
2019-03-26 17:08:53 +0800

    update

--- dqn/tracholar/dqn.py
+++ dqn/tracholar/dqn.py
@@ -10,15 +10,17 @@ import gym
 obs_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 4))
 obs_new_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 4))
 
-
 with tf.variable_scope('dqn'):
-    h1 = tf.layers.dense(obs_placeholder, 16, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(stddev=0.7), name='hidden1')
+    h1 = tf.layers.dense(obs_placeholder, 64, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(stddev=0.7), name='hidden1')
     q = tf.layers.dense(h1, 2, name='q')
+
+    tf.summary.histogram('dqn-h1', h1)
+    tf.summary.histogram('dqn-q', q)
 
 ## set target net trainable to False
 with tf.variable_scope('dqn_target'):
-    h2 = tf.layers.dense(obs_new_placeholder, 16, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(stddev=0.7), trainable=False, name='hidden1')
-    q_target = tf.layers.dense(h2, 2, trainable=False, name='q')
+    h2 = tf.layers.dense(obs_new_placeholder, 64, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(stddev=0.7), trainable=False, name='hidden1')
+    q_target = tf.layers.dense(h1, 2, trainable=False, name='q')
 
 vars1 = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='dqn')
 vars2 = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='dqn_target')
@@ -42,15 +44,16 @@ pred_q = tf.reduce_sum(q * act_onehot, axis=1)
 target_q = rew_placeholder + tf.cast(done_placeholder, dtype=tf.float32) * tf.reduce_max(q_target, axis=1)  # gamma = 1
 
 print pred_q.shape, target_q.shape
-loss = tf.reduce_sum((pred_q - target_q)**2) + 1e-2*var_w
+loss = tf.reduce_sum(tf.log1p((pred_q - target_q)**2)) + 1e-2*var_w
+
+tf.summary.scalar('loss', loss)
 
 adam = tf.train.AdamOptimizer(1e-3)
 step = tf.Variable(0, trainable=False)
 train_op = adam.minimize(loss, var_list=vars1, global_step=step)
 
-tf.GraphKeys
 class ExpBuffer():
-    def __init__(self, max_size=10240):
+    def __init__(self, max_size=102400):
         self.buff = []
         self.max_size = max_size
 
@@ -98,12 +101,16 @@ env = gym.make('CartPole-v1')
 
 observation = env.reset()
 
+merge_op = tf.summary.merge_all()
 init_op = tf.global_variables_initializer()
 avg_reward = 0
 max_reward = 0
 cum_reward = 0
 alpha = 0.95
 with tf.Session() as sess:
+    fwriter = tf.summary.FileWriter('./log/', graph=sess.graph)
+
+
     sess.run([init_op])
 
     for i in range(1024000):
@@ -138,6 +145,15 @@ with tf.Session() as sess:
         })
 
         if (i+1) % 1024 == 0:
+            summary, = sess.run([merge_op], feed_dict={
+                obs_placeholder: obs,
+                act_placeholder: act,
+                rew_placeholder: rew,
+                obs_new_placeholder: obsn,
+                done_placeholder: doneb
+            })
+            fwriter.add_summary(summary, global_step=istep)
+
             print istep, 'update target, loss =', l, 'epsilon=', epsilon,\
                 'avg_reward=', avg_reward,\
                 'max_reward=',max_reward,\
