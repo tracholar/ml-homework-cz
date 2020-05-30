9e8271c5d793bf5cac28872008e3193aa0ecb12e
zuoyuan <zuoyuan@meituan.com>
2020-05-30 18:39:52 +0800

    add mlp dot

---
+++ amazing-c/tracholar/hello.cc
@@ -0,0 +1,12 @@
+#if 0
+gcc $0 -o hello && ./hello; exit
+#endif
+
+
+#include<stdio.h>
+
+int main(){
+    printf("hello world!\n");
+}
+
+
---
+++ mlp-dot/README.md
@@ -0,0 +1,5 @@
+# 用MLP学习内积函数
+- 论文[1]指出，MLP学习内积是一件困难的事情
+
+# REF
+1. Neural Collaborative Filtering vs. Matrix Factorization Revisited
